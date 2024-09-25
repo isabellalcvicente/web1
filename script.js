c48db3b37e1659a7b9c16998572756b8a284dd98
isabellalcvicente <isabellacadastros16@gmail.com>
2024-09-25 16:35:26 -0300

    feat script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+// Selecionando pelo ID do título
+const title = document.getElementById("titulo");
+
+// Selecionando o botão que exibe "Hello"
+const helloBtn = document.getElementById("botao-ola");
+
+// Selecionando o botão que altera o título
+const changeTitleBtn = document.getElementById("btn-mudar-titulo");
+
+const mensagens = document.getElementsByClassName("mensagem2");
+
+// Evento de clique para exibir uma mensagem "Olá"
+helloBtn.addEventListener("click", function () {
+  alert("Olá!");
+});
+
+// Evento de clique para alterar o texto do título
+changeTitleBtn.addEventListener("click", function () {
+  title.textContent = "vai toma no cu";
+});
+
+//changeTitleBtn.addEventListener("click", function () {
+ // mensagens.textContent = "vai toma no cu por favor";
+//}); 
+
+const mudarFraseBtn = document.createElement('button');
+mudarFraseBtn.textContent = 'Mude as frases'; 
+document.querySelector('.container').appendChild(mudarFraseBtn); 
+
+mudarFraseBtn.addEventListener('click', function (){
+    for(let i=0; i<mensagens.length; i++){
+        mensagens[i].textContent = `mensagem ${i+1}`
+    }
+});
